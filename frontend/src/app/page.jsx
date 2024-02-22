0630701b80117e7bfd0357aaeb2198ec1bcfb0ff
Tahlil <bsse0803@iit.du.ac.bd>
2024-02-22 16:24:22 +0600

    Change home texts

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -20,23 +20,15 @@ const Homepage = () => {
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* TITLE */}
           <h1 className="text-4xl md:text-6xl font-bold">
-            Crafting Digital Experiences, Designing Tomorrow.
+            THE <span className="text-sky-500">SUI</span> Fullstack boilerplate  
           </h1>
           {/* DESC */}
           <p className="md:text-xl">
-            Welcome to my digital canvas, where innovation and creativity
-            converge. With a keen eye for aesthetics and a mastery of code, my
-            portfolio showcases a diverse collection of projects that reflect my
-            commitment to excellence.
+          The <span className="text-sky-500">SUI</span> move boilerplate coded using Next.js, Tailwind CSS, and SUI TypeScript SDK is a powerful combination of technologies for building modern web applications.
           </p>
           {/* BUTTONS */}
           <div className="w-full flex gap-4">
-            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
-              View My Work
-            </button>
-            <button className="p-4 rounded-lg ring-1 ring-black">
-              Contact Me
-            </button>
+        
           </div>
         </div>
       </div>
