e43b2b912c61c1be943fd0d5a5641a7e22fa4dd3
Tahlil <bsse0803@iit.du.ac.bd>
2024-02-24 14:04:54 +0600

    Fetch owned object

--- frontend/src/components/asset.jsx
+++ frontend/src/components/asset.jsx
@@ -2,6 +2,42 @@ import { useCurrentAccount, useSuiClientQuery } from "@mysten/dapp-kit";
 import { Flex, Heading, Text } from "@chakra-ui/react";
 
 const Asset = () => {
-    
+    const account = useCurrentAccount();
+    const { data, isPending, error } = useSuiClientQuery(
+      "getOwnedObjects",
+      {
+        owner: account?.address,
+      },
+      {
+        enabled: !!account,
+      },
+    );
+  
+    if (!account) {
+      return;
+    }
+  
+    if (error) {
+      return <Flex>Error: {error.message}</Flex>;
+    }
+  
+    if (isPending || !data) {
+      return <Flex>Loading...</Flex>;
+    }
+  
+    return (
+      <Flex direction="column" my="2">
+        {data.data.length === 0 ? (
+          <Text>No objects owned by the connected wallet</Text>
+        ) : (
+          <Heading size="4">Objects owned by the connected wallet</Heading>
+        )}
+        {data.data.map((object) => (
+          <Flex key={object.data?.objectId}>
+            <Text>Object ID: {object.data?.objectId}</Text>
+          </Flex>
+        ))}
+      </Flex>
+    );
 }
 export default Asset;
