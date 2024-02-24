2e39fc11e5c92715d89e168fb9db56240062827c
Tahlil <bsse0803@iit.du.ac.bd>
2024-02-24 14:03:48 +0600

    Create asset component

--- frontend/src/app/asset/page.jsx
+++ frontend/src/app/asset/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 import { motion, useScroll, useTransform } from "framer-motion";
-import Image from "next/image";
-import Link from "next/link";
+
 import { useRef } from "react";
 
 
---
+++ frontend/src/components/asset.jsx
@@ -0,0 +1,7 @@
+import { useCurrentAccount, useSuiClientQuery } from "@mysten/dapp-kit";
+import { Flex, Heading, Text } from "@chakra-ui/react";
+
+const Asset = () => {
+    
+}
+export default Asset;
